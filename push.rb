210b967a50cf35b3012e3f4509c99a02f6dd3141
Norton <kevin197011@outlook.com>
2025-04-28 14:34:11 +0800

    Update 2025-04-28 14:34:10.

--- push.rb
+++ push.rb
@@ -5,9 +5,4 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-require 'time'
-
-system 'git add .'
-system "git commit -m 'Update #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}.'"
-system 'git pull'
-system 'git push'
+system 'rake'

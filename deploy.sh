6586e72d9b89d9902ddba487f15ace2d0add1cd4
kk <61411931+kevin197011@users.noreply.github.com>
2025-05-02 17:42:04 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Kk
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+docker compose up -d --force-recreate --no-deps --build --remove-orphans
